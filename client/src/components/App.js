95b7d62fdf08671b3204f968d74e0fb4e833c3f2
austinb847 <austinb847@icloud.com>
2021-01-21 13:38:03 -0800

    add initial routes, and header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div className="container">
+      <BrowserRouter>
+        <Header/>
+        <Route exact path="/" />
+        <Route exact path="/surveys" />
+        <Route exact path= "/surveys/new"/>
+      </BrowserRouter>
     </div>
   );
 };
