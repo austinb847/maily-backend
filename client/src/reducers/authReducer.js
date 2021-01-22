3fca9bc85da5306c6ac2b2046268adddcb5d107e
austinb847 <austinb847@icloud.com>
2021-01-21 17:40:02 -0800

    refactor authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,11 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(state = null, action) {
+  console.log(action);
   switch(action.type) {
+    case FETCH_USER: 
+      return action.payload || false; //return the user data or return false if the payload is empty (that would mean user is not logged in)
     default:
       return state;
   }
