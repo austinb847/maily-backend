e751a686192a71547131ff0508de110a94998f72
austinb847 <austinb847@icloud.com>
2021-01-25 17:29:30 -0800

    utilize mapStateToProps to access authReducer state, and conditionally render header content based on auth state.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,12 +30,18 @@ export default class Header extends Component {
             Maily
           </a>
           <ul className="right">
-            <li>
-              <a href="/auth/google">Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps)(Header);
