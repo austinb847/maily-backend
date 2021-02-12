6da625f2bd2c7bf11056caaf078324f053c82ca6
austinb847 <austinb847@icloud.com>
2021-02-11 16:51:46 -0800

    add body parser middleware to express api in order to parse client request bodys

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  bodyParser.json()
+);
 
 app.use(
   cookieSession({
